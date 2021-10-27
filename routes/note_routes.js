cc23f95c62c6bc2b0143b50d61fd115027395934
lutakyn <32960420+lutakyn@users.noreply.github.com>
2021-10-27 06:50:03 +0000

    Update note_routes.js

--- routes/note_routes.js
+++ routes/note_routes.js
@@ -6,21 +6,21 @@ module.exports = function(app, db) {
         console.log(req.body)  
         res.send('Hello')
         console.log("db",db)
-        // db.collection('notes').insert(note, (err, result) =>{
-        //     if (err) {        
-        //          res.send({ 'error': 'An error has occurred' }); 
-        //         }
-        //     else {        
-        //         res.send(result.ops[0]);     
-        //      }   
+        db.collection('notes').insert(note, (err, result) =>{
+            if (err) {        
+                 res.send({ 'error': 'An error has occurred' }); 
+                }
+            else {        
+                res.send(result.ops[0]);     
+             }   
 
-        // });
+        });
 
     });
-    // app.post('/notes', (req, res) => {    
-    //     // You'll create your note here.    
+    app.post('/notes', (req, res) => {    
+        // You'll create your note here.    
        
-    //     console.log(req.body)  
-    //      res.send('Hello')
-    // });
+        console.log(req.body)  
+         res.send('Hello')
+    });
 }
